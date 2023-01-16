a34d0a43f097d54224d2ef93d044c1b0a4d88502
bgillcode <b.gill.messages@gmail.com>
2023-01-16 23:33:01 +0000

    Move logic for translation out of the content script

--- content.js
+++ content.js
@@ -6,29 +6,9 @@ document.body.addEventListener('mouseover', (e) => {
       e.target.style.backgroundColor = '';
     });
 
-    const API =
-      'https://translate.googleapis.com/translate_a/single?client=gtx&sl=';
-    const sourceLanguage = 'en';
-    const targetLanguage = 'ja';
-    const textContent = encodeURI(e.target.textContent);
-
-    fetch(
-      `${API}${sourceLanguage}&tl=${targetLanguage}&dt=t&q=${textContent}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data, 'data');
-      })
-      .catch((err) => {
-        console.error('Error occurred: ', err);
-      });
+    chrome.runtime.sendMessage({
+      type: 'selectedText',
+      textContent: e.target.textContent,
+    });
   }
 });
